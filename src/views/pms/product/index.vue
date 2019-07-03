1b2e23750e19f8cd268b296299f450cf6042db02
koloa-yang <185873016@qq.com>
2019-07-03 15:51:31 +0800

    first commit

---
+++ src/views/pms/product/index.vue
@@ -0,0 +1,386 @@
+<template> 
+  <div class="app-container">
+    <el-card class="filter-container" shadow="never">
+      <div>
+        <i class="el-icon-search"></i>
+        <span>筛选搜索</span>
+        <el-button
+          style="float: right"
+          @click="handleSearchList()"
+          type="primary"
+          size="small">
+          查询结果
+        </el-button>
+        <el-button
+          style="float: right;margin-right: 15px"
+          @click="handleResetSearch()"
+          size="small">
+          重置
+        </el-button>
+      </div>
+      <div style="margin-top: 15px">
+        <el-form :inline="true" :model="listQuery" size="small" label-width="140px">
+          <el-form-item label="输入搜索：">
+            <el-input style="width: 203px" v-model="listQuery.keyword" placeholder="商品名称"></el-input>
+          </el-form-item>
+          <el-form-item label="商品货号：">
+            <el-input style="width: 203px" v-model="listQuery.productSn" placeholder="商品货号"></el-input>
+          </el-form-item>
+          <el-form-item label="商品品牌：">
+            <el-select v-model="listQuery.brandId" placeholder="请选择品牌" clearable>
+              <el-option
+                v-for="item in brandOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
+    <el-card class="operate-container" shadow="never">
+      <i class="el-icon-tickets"></i>
+      <span>数据列表</span>
+      <el-button
+        class="btn-add"
+        @click="handleAddProduct()"
+        size="mini">
+        添加
+      </el-button>
+    </el-card>
+    <div class="table-container">
+      <el-table ref="productTable"
+                :data="listdata"
+                style="width: 100%"
+                @selection-change="handleSelectionChange"
+                border>
+        <el-table-column type="selection" width="100" align="center"></el-table-column>
+        <el-table-column label="编号" width="120" align="center">
+          <template slot-scope="scope">{{scope.row.id}}</template>
+        </el-table-column>
+        <el-table-column label="商品图片" width="160" align="center">
+          <template slot-scope="scope"><img style="height: 80px" :src="scope.row.pic"></template>
+        </el-table-column>
+        <el-table-column label="商品名称" align="center">
+          <template slot-scope="scope">
+            <p>{{scope.row.name}}</p>
+            <p>品牌：{{scope.row.brandName}}</p>
+          </template>
+        </el-table-column>
+        <el-table-column label="价格/货号" width="160" align="center">
+          <template slot-scope="scope">
+            <p>价格：￥{{scope.row.price}}</p>
+            <p>货号：{{scope.row.productSn}}</p>
+          </template>
+        </el-table-column>
+        <el-table-column label="类别" width="120" align="center">
+          <template slot-scope="scope">{{scope.row.type}}</template>
+        </el-table-column>
+        <el-table-column label="销量" width="120" align="center">
+          <template slot-scope="scope">{{scope.row.sale}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template slot-scope="scope">
+            <p>
+              <el-button
+                size="mini"
+                @click="handleUpdateProduct(scope.$index, scope.row)">编辑
+              </el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除
+              </el-button>
+            </p>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="pagination-container">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        layout="total, sizes,prev, pager, next,jumper"
+        :page-size="listQuery.pageSize"
+        :page-sizes="[5,10,15]"
+        :current-page.sync="listQuery.pageNum"
+        :total="total">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+<script>
+  import {
+    fetchList,
+    updateDeleteStatus,
+    updateNewStatus,
+    updateRecommendStatus,
+    updatePublishStatus
+  } from '@/api/product'
+  import {fetchList as fetchBrandList} from '@/api/brand'
+  import {fetchListWithChildren} from '@/api/productCate'
+  // import {fetchList as fetchSkuStockList,update as updateSkuStockList} from '@/api/skuStock'
+  // import {fetchList as fetchProductAttrList} from '@/api/productAttr'
+
+
+  const defaultListQuery = {
+    keyword: null,
+    pageNum: 1,
+    pageSize: 5,
+    publishStatus: null,
+    verifyStatus: null,
+    productSn: null,
+    productCategoryId: null,
+    brandId: null
+  };
+  export default {
+    name: "productList",
+    data() {
+      return {
+        listdata:[{
+          id: '1',
+          pic: '1',
+          name: 'name',
+          brandName: 'HaiEr',
+          price: 100,
+          type: '科幻',
+          productSn: 'A1',
+          sale:53
+        }],
+        editSkuInfo:{
+          dialogVisible:false,
+          productId:null,
+          productSn:'',
+          productAttributeCategoryId:null,
+          stockList:[],
+          productAttr:[],
+          keyword:null
+        },
+        operates: [
+          {
+            label: "商品上架",
+            value: "publishOn"
+          },
+          {
+            label: "商品下架",
+            value: "publishOff"
+          },
+          {
+            label: "设为推荐",
+            value: "recommendOn"
+          },
+          {
+            label: "取消推荐",
+            value: "recommendOff"
+          },
+          {
+            label: "设为新品",
+            value: "newOn"
+          },
+          {
+            label: "取消新品",
+            value: "newOff"
+          },
+          {
+            label: "转移到分类",
+            value: "transferCategory"
+          },
+          {
+            label: "移入回收站",
+            value: "recycle"
+          }
+        ],
+        operateType: null,
+        listQuery: Object.assign({}, defaultListQuery),
+        list: null,
+        total: null,
+        listLoading: true,
+        selectProductCateValue: null,
+        multipleSelection: [],
+        productCateOptions: [],
+        brandOptions: [],
+        publishStatusOptions: [{
+          value: 1,
+          label: '上架'
+        }, {
+          value: 0,
+          label: '下架'
+        }],
+        verifyStatusOptions: [{
+          value: 1,
+          label: '审核通过'
+        }, {
+          value: 0,
+          label: '未审核'
+        }]
+      }
+    },
+    created() {
+      // this.getList();
+      // this.getBrandList();
+      // this.getProductCateList();
+    },
+    watch: {
+      selectProductCateValue: function (newValue) {
+        if (newValue != null && newValue.length == 2) {
+          this.listQuery.productCategoryId = newValue[1];
+        } else {
+          this.listQuery.productCategoryId = null;
+        }
+
+      }
+    },
+    filters: {
+      verifyStatusFilter(value) {
+        if (value === 1) {
+          return '审核通过';
+        } else {
+          return '未审核';
+        }
+      }
+    },
+    methods: {
+      getProductSkuSp(row, index) {
+        if (index === 0) {
+          return row.sp1;
+        } else if (index === 1) {
+          return row.sp2;
+        } else {
+          return row.sp3;
+        }
+      },
+      getList() {
+        this.listLoading = true;
+        fetchList(this.listQuery).then(response => {
+          this.listLoading = false;
+          this.list = response.data.list;
+          this.total = response.data.total;
+        });
+      },
+      getBrandList() {
+        fetchBrandList({pageNum: 1, pageSize: 100}).then(response => {
+          this.brandOptions = [];
+          let brandList = response.data.list;
+          for (let i = 0; i < brandList.length; i++) {
+            this.brandOptions.push({label: brandList[i].name, value: brandList[i].id});
+          }
+        });
+      },
+      getProductCateList() {
+        fetchListWithChildren().then(response => {
+          let list = response.data;
+          this.productCateOptions = [];
+          for (let i = 0; i < list.length; i++) {
+            let children = [];
+            if (list[i].children != null && list[i].children.length > 0) {
+              for (let j = 0; j < list[i].children.length; j++) {
+                children.push({label: list[i].children[j].name, value: list[i].children[j].id});
+              }
+            }
+            this.productCateOptions.push({label: list[i].name, value: list[i].id, children: children});
+          }
+        });
+      },
+      handleSearchList() {
+        this.listQuery.pageNum = 1;
+        this.getList();
+      },
+      handleAddProduct() {
+        this.$router.push({path:'/pms/addProduct'});
+      },
+      handleSizeChange(val) {
+        this.listQuery.pageNum = 1;
+        this.listQuery.pageSize = val;
+        this.getList();
+      },
+      handleCurrentChange(val) {
+        this.listQuery.pageNum = val;
+        this.getList();
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      handleResetSearch() {
+        this.selectProductCateValue = [];
+        this.listQuery = Object.assign({}, defaultListQuery);
+      },
+      handleDelete(index, row){
+        this.$confirm('是否要进行删除操作?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          let ids = [];
+          ids.push(row.id);
+          this.updateDeleteStatus(1,ids);
+        });
+      },
+      handleUpdateProduct(index,row){
+        this.$router.push({path:'/pms/updateProduct',query:{id:row.id}});
+      },
+      handleShowProduct(index,row){
+        console.log("handleShowProduct",row);
+      },
+      handleShowVerifyDetail(index,row){
+        console.log("handleShowVerifyDetail",row);
+      },
+      handleShowLog(index,row){
+        console.log("handleShowLog",row);
+      },
+      // updatePublishStatus(publishStatus, ids) {
+      //   let params = new URLSearchParams();
+      //   params.append('ids', ids);
+      //   params.append('publishStatus', publishStatus);
+      //   updatePublishStatus(params).then(response => {
+      //     this.$message({
+      //       message: '修改成功',
+      //       type: 'success',
+      //       duration: 1000
+      //     });
+      //   });
+      // },
+      // updateNewStatus(newStatus, ids) {
+      //   let params = new URLSearchParams();
+      //   params.append('ids', ids);
+      //   params.append('newStatus', newStatus);
+      //   updateNewStatus(params).then(response => {
+      //     this.$message({
+      //       message: '修改成功',
+      //       type: 'success',
+      //       duration: 1000
+      //     });
+      //   });
+      // },
+      // updateRecommendStatus(recommendStatus, ids) {
+      //   let params = new URLSearchParams();
+      //   params.append('ids', ids);
+      //   params.append('recommendStatus', recommendStatus);
+      //   updateRecommendStatus(params).then(response => {
+      //     this.$message({
+      //       message: '修改成功',
+      //       type: 'success',
+      //       duration: 1000
+      //     });
+      //   });
+      // },
+      updateDeleteStatus(deleteStatus, ids) {
+        let params = new URLSearchParams();
+        params.append('ids', ids);
+        params.append('deleteStatus', deleteStatus);
+        updateDeleteStatus(params).then(response => {
+          this.$message({
+            message: '删除成功',
+            type: 'success',
+            duration: 1000
+          });
+        });
+        this.getList();
+      }
+    }
+  }
+</script>
+<style></style>
+
+
